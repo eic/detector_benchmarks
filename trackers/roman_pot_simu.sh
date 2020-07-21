32bc943fe5f05811c9d32d6048b3d8581d219ed1
Tomas Polakovic <tom.polakovic@gmail.com>
2020-07-21 16:25:03 -0500

    Stupid filepaths.

--- trackers/roman_pot_simu.sh
+++ trackers/roman_pot_simu.sh
@@ -3,6 +3,6 @@
 source /usr/local/bin/thisdd4hep.sh
 
 ddsim --runType batch -N 300 \
-	--inputFiles ../data/forward_ions.hepmc \
-	--compactFile ./roman_pot.xml \
-	--outputFile ../sim_output/roman_pot_out.root
+	--inputFiles ./data/forward_ions.hepmc \
+	--compactFile ./trackers/roman_pot.xml \
+	--outputFile ./sim_output/roman_pot_out.root

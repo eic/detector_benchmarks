b85b50b5bf6f25e6b191b37e4babcb266ad2c48d
jihee.kim <jihee.kim@anl.gov>
2020-07-23 12:11:45 -0500

    Added source /usr/local/bin/thisdd4hep.sh.

--- calorimeters/run_simulation_zdc.sh
+++ calorimeters/run_simulation_zdc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source /usr/local/bin/thisdd4hep.sh
+
 ddsim --runType batch --numberOfEvents 100 \
       --compactFile ./ZDC_example.xml \
       --inputFiles  ./data/zdc_photons.hepmc \

b0c732e5cd7735783e5ffb935897889ae5b5d3c2
jihee.kim <jihee.kim@anl.gov>
2020-07-23 12:28:40 -0500

    Corrected a path of where compact file is located as Tom pointed out.

--- calorimeters/run_simulation_zdc.sh
+++ calorimeters/run_simulation_zdc.sh
@@ -3,6 +3,6 @@
 source /usr/local/bin/thisdd4hep.sh
 
 ddsim --runType batch --numberOfEvents 100 \
-      --compactFile ./ZDC_example.xml \
+      --compactFile ./calorimeters/ZDC_example.xml \
       --inputFiles  ./data/zdc_photons.hepmc \
       --outputFile  ./sim_output/output_zdc_photons.root

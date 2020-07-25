ecb748fd371de70ed8a5f32ea257d5b047cf0be1
Sylvester Joosten <sjoosten@anl.gov>
2020-07-25 14:55:33 -0500

    Updated CI for v2.0.2-based container

--- calorimeters/run_simulation_zdc.sh
+++ calorimeters/run_simulation_zdc.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source /etc/profile
-
 ddsim --runType batch --numberOfEvents 100 \
       --compactFile ./calorimeters/ZDC_example.xml \
       --inputFiles  ./data/zdc_photons.hepmc \

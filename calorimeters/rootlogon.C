b12b323e2436414e69a94ef98d389458288a10fe
jihee.kim <jihee.kim@anl.gov>
2020-07-27 11:51:49 -0500

    Added rootlogon.C.

---
+++ calorimeters/rootlogon.C
@@ -0,0 +1,11 @@
+{
+	// Setting for Graphs
+	gROOT->SetStyle("Plain");
+	gStyle->SetOptFit(1);
+	gStyle->SetLineWidth(2);
+	gStyle->SetPadTickX(1);
+	gStyle->SetPadTickY(1);
+	gStyle->SetPadGridX(1);
+	gStyle->SetPadGridY(1);
+	gStyle->SetPadLeftMargin(0.14);
+}

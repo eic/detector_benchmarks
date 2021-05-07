8270a1a330c4fa928adcb2875becf8c8938b52ef
Marshall Scott <mbscott@anl.gov>
2021-05-07 14:08:49 -0400

    Changed pid function 2

--- benchmarks/barrel_ecal/scripts/emcal_barrel_pions_electrons_analysis.cxx
+++ benchmarks/barrel_ecal/scripts/emcal_barrel_pions_electrons_analysis.cxx
@@ -70,7 +70,8 @@ void emcal_barrel_pions_electrons_analysis(const char* input_fname = "sim_output
 
   // Returns the pdgID of the particle
   auto getpid = [](std::vector<dd4pod::Geant4ParticleData> const& input) {
-    return {input[2].pdgID, input[3].pdgID};
+    std::vector<double> result = {input[2].pdgID, input[3].pdgID};
+    return result;
   };
 
   // Returns number of particle daughters

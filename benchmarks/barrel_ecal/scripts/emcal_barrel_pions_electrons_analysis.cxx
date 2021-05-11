a6b8ea9af95b6c360d5abf60afe88e7c0939d9fb
Marshall Scott <mbscott@anl.gov>
2021-05-11 09:29:01 -0400

    Cleaning up plots

--- benchmarks/barrel_ecal/scripts/emcal_barrel_pions_electrons_analysis.cxx
+++ benchmarks/barrel_ecal/scripts/emcal_barrel_pions_electrons_analysis.cxx
@@ -16,6 +16,7 @@
 #include "TF1.h"
 #include "TH1D.h"
 #include "TFitResult.h"
+#include "TLegend.h"
 
 R__LOAD_LIBRARY(libfmt.so)
 #include "fmt/core.h"
@@ -85,40 +86,11 @@ void emcal_barrel_pions_electrons_analysis(const char* input_fname = "sim_output
   return result;
   };
 
-  // Energy deposition electron
-  auto Esim_ele = [](const std::vector<dd4pod::CalorimeterHitData>& evt, std::vector<dd4pod::Geant4ParticleData> const& input) {
-    std::vector<double> result;
-    auto total_edep = 0.0;
-    int count = 0;
-    if (input[2].pdgID == 11)// Electron
-    { 
-      for (const auto& i: evt)
-        total_edep += i.energyDeposit;
-        count++;
-    }
-    result.push_back(total_edep);
-  return result;
-  };
-
-  // Energy deposition [GeV] pion
-  auto Esim_pi = [](const std::vector<dd4pod::CalorimeterHitData>& evt, std::vector<dd4pod::Geant4ParticleData> const& input) {
-    std::vector<double> result;
-    auto total_edep = 0.0;
-    int count = 0;
-    if (input[2].pdgID == -211)// Negative pion
-    { 
-      for (const auto& i: evt)
-        total_edep += i.energyDeposit;
-        count++;
-    }
-    result.push_back(total_edep);
-  return result;
-  };
-
+  // Energy deposititon [GeV] in the first 4 layers
   auto Esim_front = [=](const std::vector<dd4pod::CalorimeterHitData>& evt) {
     auto total_edep = 0.0;
     for (const auto& i: evt) {
-      fmt::print("cell id {}, layer {}\n",i.cellID, decoder->get(i.cellID, layer_index));
+      //fmt::print("cell id {}, layer {}\n",i.cellID, decoder->get(i.cellID, layer_index));
       if( decoder->get(i.cellID, layer_index) < 4 ){
         total_edep += i.energyDeposit;
       }
@@ -139,7 +111,6 @@ void emcal_barrel_pions_electrons_analysis(const char* input_fname = "sim_output
 
   // Returns the pdgID of the particle
   auto getpid = [](std::vector<dd4pod::Geant4ParticleData> const& input) {
-    //std::vector<int> result = {input[2].pdgID, input[3].pdgID};
     return input[2].pdgID;
   };
 
@@ -148,8 +119,15 @@ void emcal_barrel_pions_electrons_analysis(const char* input_fname = "sim_output
     return input[2].daughters_begin;
   };
 
+  // Filter function to get electrons
   auto is_electron = [](std::vector<dd4pod::Geant4ParticleData> const& input){
     if (input[2].pdgID == 11){return true;}
+    else {return false;}
+  };
+
+  // Filter function to get just negative pions
+  auto is_piMinus = [](std::vector<dd4pod::Geant4ParticleData> const& input){
+    if (input[2].pdgID == -211){return true;}
     else {return false;}
   };
 
@@ -167,25 +145,27 @@ void emcal_barrel_pions_electrons_analysis(const char* input_fname = "sim_output
               .Define("Esim_front", Esim_front, {"EcalBarrelHits"})
               ;
 
-  auto d2 = d1.Filter(is_electron, {"mcparticles"});
+  auto d_ele = d1.Filter(is_electron, {"mcparticles"});
+  auto d_pim = d1.Filter(is_piMinus,  {"mcparticles"});
 
   // Define Histograms
-  auto hEthr  = d1.Histo1D({"hEthr",  "Thrown Energy; Thrown Energy [GeV]; Events",        100,  0.0,    7.5}, "Ethr");
-  auto hNhits = d1.Histo1D({"hNhits", "Number of hits per events; Number of hits; Events", 100,  0.0, 2000.0}, "nhits");
-  auto hEsim  = d1.Histo1D({"hEsim",  "Energy Deposit; Energy Deposit [GeV]; Events",      10,  0.0,    0.5}, "Esim");
-  auto hfsam  = d1.Histo1D({"hfsam",  "Sampling Fraction; Sampling Fraction; Events",      100,  0.0,    0.1}, "fsam");
-  auto hpid   = d1.Histo1D({"hpid",   "PID; PID; Count",                                   100,  -220,   220}, "pid");
-  auto hdau   = d1.Histo1D({"hdau",   "Number of Daughters; Number of Daughters; Count",   10,   0,      10},  "dau");
+  auto hEthr       = d1.Histo1D({"hEthr",  "Thrown Energy; Thrown Energy [GeV]; Events",                            100,  0.0,    7.5}, "Ethr");
+  auto hNhits      = d1.Histo1D({"hNhits", "Number of hits per events; Number of hits; Events",                     100,  0.0, 2000.0}, "nhits");
+  auto hEsim       = d1.Histo1D({"hEsim",  "Energy Deposit; Energy Deposit [GeV]; Events",                           10,  0.0,    0.5}, "Esim");
+  auto hfsam       = d1.Histo1D({"hfsam",  "Sampling Fraction; Sampling Fraction; Events",                          100,  0.0,    0.1}, "fsam");
+  auto hEsim_front = d1.Histo1D({"hEsim_front",  "Energy Deposit Front Electron; Energy Deposit [GeV]; Events",      10,  0.0,    0.5}, "Esim_front");
 
-  auto hEsim_ele        = d2.Histo1D({"hEsim_ele",        "Energy Deposit Electron; Energy Deposit [GeV]; Events",      10,  0.0,    0.5}, "Esim");
-  auto hEsim_ele_front  = d2.Histo1D({"hEsim_ele_front",  "Energy Deposit Front Electron; Energy Deposit [GeV]; Events",      10,  0.0,    0.5}, "Esim_front");
-  auto hEsim_front      = d1.Histo1D({"hEsim_front",      "Energy Deposit Front Electron; Energy Deposit [GeV]; Events",      10,  0.0,    0.5}, "Esim_front");
+  auto hEsim_ele        = d_ele.Histo1D({"hEsim_ele",        "Energy Deposit Electron; Energy Deposit [GeV]; Events",            10,  0.0,    0.5}, "Esim");
+  auto hEsim_ele_front  = d_ele.Histo1D({"hEsim_ele_front",  "Energy Deposit Front Electron; Energy Deposit [GeV]; Events",      10,  0.0,    0.5}, "Esim_front");
+  
 
   TH1D* hElePurity_initial = (TH1D *)hEsim_ele -> Clone();
   hElePurity_initial -> Divide(hEsim.GetPtr());
 
   TH1D* hElePurity_final = (TH1D *)hEsim_ele_front -> Clone();
   hElePurity_initial -> Divide(hEsim_front.GetPtr());
+
+  hElePurity_initial -> SetTitle("Electron/Pion Rejection");
 
 
   // Event Counts
@@ -201,15 +181,6 @@ void emcal_barrel_pions_electrons_analysis(const char* input_fname = "sim_output
   hEthr->DrawClone();
   c1->SaveAs("results/emcal_barrel_pions_electrons_Ethr.png");
   c1->SaveAs("results/emcal_barrel_pions_electrons_Ethr.pdf");
-
-  TCanvas *c2 = new TCanvas("c2", "c2", 700, 500);
-  c2->SetLogy(1);
-  hNhits->GetYaxis()->SetTitleOffset(1.4);
-  hNhits->SetLineWidth(2);
-  hNhits->SetLineColor(kBlue);
-  hNhits->DrawClone();
-  c2->SaveAs("results/emcal_barrel_pions_electrons_nhits.png");
-  c2->SaveAs("results/emcal_barrel_pions_electrons_nhits.pdf");
 
   TCanvas *c3 = new TCanvas("c3", "c3", 700, 500);
   c3->SetLogy(1);
@@ -228,24 +199,10 @@ void emcal_barrel_pions_electrons_analysis(const char* input_fname = "sim_output
   hEsim_ele->DrawClone();
   c4->SaveAs("results/emcal_barrel_pions_electrons_Esim_ele.png");
   c4->SaveAs("results/emcal_barrel_pions_electrons_Esim_ele.pdf");
-/*
-  TCanvas *c5 = new TCanvas("c5", "c5", 700, 500);
-  c5->SetLogy(1);
-  hEsim_pi->GetYaxis()->SetTitleOffset(1.4);
-  hEsim_pi->SetLineWidth(2);
-  hEsim_pi->SetLineColor(kBlue);
-  hEsim_pi->DrawClone();
-  c5->SaveAs("results/emcal_barrel_pions_electrons_Esim_pi.png");
-  c5->SaveAs("results/emcal_barrel_pions_electrons_Esim_pi.pdf");
-*/
-  TCanvas *c6 = new TCanvas("c6", "c6", 700, 500);
-  c6->SetLogy(1);
-  hpid->GetYaxis()->SetTitleOffset(1.4);
-  hpid->SetLineWidth(2);
-  hpid->SetLineColor(kBlue);
-  hpid->DrawClone();
-  c6->SaveAs("results/emcal_barrel_pions_electrons_pid.png");
-  c6->SaveAs("results/emcal_barrel_pions_electrons_pid.pdf");
+
+  auto leg = new TLegend(0.7, 0.7, 0.9, 0.9);
+  leg->AddEntry(hElePurity_initial, "Initial", "l");
+  leg->AddEntry(hElePurity_final, "Final", "l");
 
   TCanvas *c7 = new TCanvas("c7", "c7", 700, 500);
   //c7->SetLogy(1);
@@ -256,8 +213,9 @@ void emcal_barrel_pions_electrons_analysis(const char* input_fname = "sim_output
   hElePurity_final->SetLineWidth(2);
   hElePurity_final->SetLineColor(kRed);
   hElePurity_final->DrawClone("SAME");
-  c7->SaveAs("results/emcal_barrel_pions_electrons_purity.png");
-  c7->SaveAs("results/emcal_barrel_pions_electrons_purity.pdf");
+  leg->Draw("same");
+  c7->SaveAs("results/emcal_barrel_pions_electrons_rejection.png");
+  c7->SaveAs("results/emcal_barrel_pions_electrons_rejection.pdf");
 
 
 }

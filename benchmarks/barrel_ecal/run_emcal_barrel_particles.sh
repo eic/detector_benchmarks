34ce12baef66bf52f650b75804042011b553c426
Marshall Scott <mbscott@anl.gov>
2021-07-21 11:06:07 -0500

    rebasing

--- benchmarks/barrel_ecal/run_emcal_barrel_particles.sh
+++ benchmarks/barrel_ecal/run_emcal_barrel_particles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ ! -n  "${JUGGLER_DETECTOR}" ]] ; then 
-  export JUGGLER_DETECTOR="athena"
+  export JUGGLER_DETECTOR="topside"
 fi
 
 if [[ ! -n  "${JUGGLER_N_EVENTS}" ]] ; then 

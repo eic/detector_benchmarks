396f645bad85eb998365ec94cc9e0e5be4f659aa
Whitney Armstrong <warmstrong@anl.gov>
2021-08-09 09:11:14 +0000

    B0tracker basics

---
+++ benchmarks/b0_tracker/forward_protons.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+source .local/bin/env.sh
+
+if [[ ! -n  "${JUGGLER_DETECTOR}" ]] ; then 
+  export JUGGLER_DETECTOR="topside"
+fi
+
+if [[ ! -n  "${JUGGLER_N_EVENTS}" ]] ; then 
+  export JUGGLER_N_EVENTS=100
+fi
+
+export FILE_NAME_TAG="forward_protons"
+export JUGGLER_GEN_FILE="${FILE_NAME_TAG}.hepmc"
+export JUGGLER_SIM_FILE="sim_${FILE_NAME_TAG}.root"
+export JUGGLER_REC_FILE="rec_${FILE_NAME_TAG}.root"
+
+echo "JUGGLER_N_EVENTS = ${JUGGLER_N_EVENTS}"
+echo "JUGGLER_DETECTOR = ${JUGGLER_DETECTOR}"
+
+# Generate the input events
+root -b -q "benchmarks/b0_tracker/scripts/gen_${FILE_NAME_TAG}.cxx(${JUGGLER_N_EVENTS}, \"${FILE_NAME_TAG}.hepmc\")"
+if [[ "$?" -ne "0" ]] ; then
+  echo "ERROR running script: generating input events"
+  exit 1
+fi
+
+# Run geant4 simulations
+npsim --runType batch \
+      -v WARNING \
+      --part.minimalKineticEnergy 0.5*GeV  \
+      --numberOfEvents ${JUGGLER_N_EVENTS} \
+      --compactFile ${DETECTOR_PATH}/${JUGGLER_DETECTOR}.xml \
+      --inputFiles ${FILE_NAME_TAG}.hepmc \
+      --outputFile sim_output/${JUGGLER_SIM_FILE}
+
+if [[ "$?" -ne "0" ]] ; then
+  echo "ERROR running npdet"
+  exit 1
+fi
+
+# Directory for plots
+mkdir -p results
+
+# Plot the input events
+root -b -q "benchmarks/b0_tracker/scripts/b0_tracker_hits.cxx(\"sim_output/sim_${FILE_NAME_TAG}.root\")"
+if [[ "$?" -ne "0" ]] ; then
+  echo "ERROR running script: events"
+  exit 1
+fi
+
---
+++ benchmarks/b0_tracker/scripts/b0_tracker_hits.cxx
@@ -0,0 +1,64 @@
+R__LOAD_LIBRARY(libfmt.so)
+#include "fmt/core.h"
+
+#include "ROOT/RDataFrame.hxx"
+#include "Math/Vector3D.h"
+//#include "Math/Vector4D.h"
+//#include "Math/VectorUtil.h"
+#include "TCanvas.h"
+//#include "TLegend.h"
+//#include "TMath.h"
+//#include "TRandom3.h"
+//#include "TFile.h"
+//#include "TH1F.h"
+//#include "TH1D.h"
+//#include "TTree.h"
+#include "TChain.h"
+//#include "TF1.h"
+
+#include "dd4pod/TrackerHitCollection.h"
+
+#include "common_bench/particles.h"
+#include "common_bench/benchmark.h"
+#include "common_bench/mt.h"
+#include "common_bench/util.h"
+
+
+#include "dd4pod/TrackerHitCollection.h"
+
+void b0_tracker_hits(const char* fname = "./sim_output/sim_forward_protons.root"){
+
+  ROOT::EnableImplicitMT(); // Tell ROOT you want to go parallel
+  double degree = TMath::Pi()/180.0;
+
+  TChain* t = new TChain("events");
+  t->Add(fname);
+
+  ROOT::RDataFrame d0(*t);
+
+  auto hits_eta = [&](const std::vector<dd4pod::TrackerHitData>& hits) {
+    std::vector<double> result;
+    for (const auto& h : hits) {
+      ROOT::Math::XYZVector vec(h.position.x,h.position.y,h.position.z);
+      result.push_back(vec.eta());
+      std::cout << vec.eta() << "\n";
+    }
+    return result;
+  };
+
+  auto d1 = d0.Define("hits_eta", hits_eta, {"B0TrackerHits"});
+
+  auto h1 = d1.Histo1D({"h1", "hits_eta", 100, 0,20}, "hits_eta");
+  TCanvas* c = new TCanvas();
+  h1->DrawCopy();
+  c->SaveAs("results/b0_tracker_hits_eta.png");
+  c->SaveAs("results/b0_tracker_hits_eta.pdf");
+  auto n1 = h1->GetMean();
+  std::cout << "Pseudorapidity of hits: " << n1 << std::endl;
+
+  //if (n1 < 5) {
+  //        std::quick_exit(1);
+  //}
+
+}
+

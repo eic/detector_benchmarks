63699efc9b7be2813047d616d387545673c2df86
Wouter Deconinck <wdconinc@gmail.com>
2022-09-19 19:05:48 -0500

    fix: switch back to single hit collections

--- benchmarks/tracking_detectors/analysis/sim_track_hits.cxx
+++ benchmarks/tracking_detectors/analysis/sim_track_hits.cxx
@@ -113,16 +113,9 @@ int sim_track_hits(const char* fname = "sim_track_hits.edm4hep.root") {
 
   // minimal hit collection setup
   std::vector<std::pair<std::string, std::string>> hitCollections{{"vtx_barrel", "VertexBarrelHits"},
-                                                                  {"trk_barrel_outer", "OuterSiBarrelHits"},
-                                                                  {"trk_barrel_sagitta", "SagittaSiBarrelHits"},
-                                                                  {"trk_endcap_p_inner", "InnerTrackerEndcapPHits"},
-                                                                  {"trk_endcap_p_middle", "MiddleTrackerEndcapPHits"},
-                                                                  {"trk_endcap_p_outer", "OuterTrackerEndcapPHits"},
-                                                                  {"trk_endcap_n_inner", "InnerTrackerEndcapNHits"},
-                                                                  {"trk_endcap_n_middle", "MiddleTrackerEndcapNHits"},
-                                                                  {"trk_endcap_n_outer", "OuterTrackerEndcapNHits"},
-                                                                  {"mpgd_barrel_outer", "OuterMPGDBarrelHits"},
-                                                                  {"mpgd_barrel_inner", "InnerMPGDBarrelHits"}
+                                                                  {"si_barrel", "SiBarrelHits"},
+                                                                  {"trk_endcap", "TrackerEndcapHits"},
+                                                                  {"mpgd_barrel", "MPGDBarrelHits"},
   };
 
   auto df0 = df.Define("isThrown", "MCParticles.generatorStatus == 1")

d3666a987c625d1bc63ca6724b56084559c45066
Shyam Kumar <shyam055119@gmail.com>
2024-06-07 09:06:05 +0200

    Minor fix in the EICRecon script

--- benchmarks/tracking_performances/Script_widebin.sh
+++ benchmarks/tracking_performances/Script_widebin.sh
@@ -24,11 +24,10 @@ eicrecon \
  -Pnthreads=1 \
  -Pjana:debug_plugin_loading=1 \
  -Pjana:nevents=$nevents \
- -PTracking:CentralTrackerSourceLinker:input_tags=SiBarrelTrackerRecHits,SiBarrelVertexRecHits,SiEndcapTrackerRecHits,TOFBarrelRecHit,TOFEndcapRecHits,MPGDBarrelRecHits,MPGDDIRCRecHits,OuterMPGDBarrelRecHits,BackwardMPGDEndcapRecHits,ForwardMPGDEndcapRecHits \
  -Pacts:MaterialMap=calibrations/materials-map.cbor \
  -Ppodio:output_file="${filename}"_${mom_array[i]}.edm4eic.root \
  -Pdd4hep:xml_files=../epic/install/share/epic/epic_craterlake_tracking_only.xml   \
- -Ppodio:output_include_collections="ReconstructedChargedParticles,ReconstructedSeededChargedParticles,MCParticles,CentralCKFTrajectories,CentralCKFTrackParameters,CentralCKFSeededTrackParameters,CentralTrackVertices" \
+ -Ppodio:output_collections="ReconstructedChargedParticles,ReconstructedSeededChargedParticles,MCParticles,CentralCKFTrajectories,CentralCKFTrackParameters,CentralCKFSeededTrackParameters,CentralTrackVertices" \
  sim${mom_array[i]}.edm4hep.root
 done 
 

32aaaef87877ad1afcb77bc77cca60eef73105e6
simonge <simon.gardner@glasgow.ac.uk>
2024-09-16 13:12:53 +0100

    Updates

--- benchmarks/LOWQ2/reconstruction_training/TaggerRegression.py
+++ benchmarks/LOWQ2/reconstruction_training/TaggerRegression.py
@@ -7,11 +7,13 @@ from RegressionModel import makeModel, trainModel
 
 # Parse arguments
 parser = argparse.ArgumentParser(description='Train a regression model for the Tagger.')
-parser.add_argument('dataFiles', type=str, nargs='+', help='Path to the data files')
-parser.add_argument('beamEnergy', type=float, help='Electron beam energy')
+parser.add_argument('--dataFiles', type=str, nargs='+', help='Path to the data files')
+parser.add_argument('--beamEnergy', type=float, help='Electron beam energy')
+parser.add_argument('--outModelFile', type=str, default="regression_model.onnx", help='Output file for the trained model')
 args = parser.parse_args()
 dataFiles = args.dataFiles
 beamEnergy = args.beamEnergy
+outModelFile = args.outModelFile
 
 input_data, target_data = create_arrays(dataFiles, beamEnergy)
 print(input_data)
@@ -28,14 +30,14 @@ val_target_data = torch_target_data[split_index:]
 torch_input_data = torch_input_data[:split_index]
 torch_target_data = torch_target_data[:split_index] 
 
-epochs = 1000
+epochs = 5000
 model = trainModel(epochs, torch_input_data, torch_target_data, val_input_data, val_target_data)
 
 # Save the trained model to ONNX format
 dummy_input = torch_input_data[0].unsqueeze(0)  # Create a dummy input for the model
-onnx_file_path = "regression_model.onnx"
-torch.onnx.export(model, dummy_input, onnx_file_path, 
+
+torch.onnx.export(model, dummy_input, outModelFile, 
                   input_names=['input'], output_names=['output'],
                   dynamic_axes={'input': {0: 'batch_size'}, 'output': {0: 'batch_size'}})
 
-print(f"Model has been saved to {onnx_file_path}")
+print(f"Model has been saved to {outModelFile}")
--- benchmarks/LOWQ2/reconstruction_training/TestModel.py
+++ benchmarks/LOWQ2/reconstruction_training/TestModel.py
@@ -6,19 +6,24 @@ import matplotlib.pyplot as plt
 
 # Parse arguments
 parser = argparse.ArgumentParser(description='Train a regression model for the Tagger.')
-parser.add_argument('dataFiles', type=str, nargs='+', help='Path to the data files')
-parser.add_argument('beamEnergy', type=float, help='Electron beam energy')
+parser.add_argument('--modelFile', type=str, default="regression_model.onnx", help='Path to the ONNX model file')
+parser.add_argument('--dataFiles', type=str, nargs='+', help='Path to the data files')
+parser.add_argument('--beamEnergy', type=float, help='Electron beam energy')
+parser.add_argument('--outGraphFile', type=str, default="output_vs_target.png", help='Output file for the graph')
+parser.add_argument('--outGraphFile2', type=str, default="output_vs_target2.png", help='Output file for the graph')
 args = parser.parse_args()
-dataFiles = args.dataFiles
-beamEnergy = args.beamEnergy
+modelFile     = args.modelFile
+dataFiles     = args.dataFiles
+beamEnergy    = args.beamEnergy
+outGraphFile  = args.outGraphFile
+outGraphFile2 = args.outGraphFile2
 
 input_data, target_data = create_arrays(dataFiles, beamEnergy)
 
 target_data = np.array(target_data)
 
 # Load the ONNX model
-onnx_file_path = "regression_model.onnx"
-session = ort.InferenceSession(onnx_file_path)
+session = ort.InferenceSession(modelFile)
 
 # Run the model on the input data
 input_name = session.get_inputs()[0].name
@@ -26,15 +31,29 @@ output_name = session.get_outputs()[0].name
 input_data = np.array(input_data,dtype=np.float32)
 output = session.run([output_name], {input_name: input_data})
 output = np.array(output[0])#*beamEnergy
-print(output)
+#print(output)
+
+out_theta = np.arctan2(np.sqrt(output[:,0]**2 + output[:,1]**2),output[:,2])
+out_phi = np.arctan2(output[:,1],output[:,0])
+out_mag = np.sqrt(output[:,0]**2 + output[:,1]**2 + output[:,2]**2)
+in_theta = np.arctan2(np.sqrt(target_data[:,0]**2 + target_data[:,1]**2),target_data[:,2])
+in_phi = np.arctan2(target_data[:,1],target_data[:,0])
+in_mag = np.sqrt(target_data[:,0]**2 + target_data[:,1]**2 + target_data[:,2]**2)
+
+
+thetadiff = out_theta - in_theta
+phidiff = out_phi - in_phi
+# Move phidiff to within -pi/2 and pi/2
+phidiff = (phidiff + np.pi) % (2 * np.pi) - np.pi
+magdiff = out_mag - in_mag
 
 diff = (target_data - output)/target_data
-diffrange = [[-2,2],[-2,2],[-2,2]]
+diffrange = [[-5,5],[-5,5],[-2,2]]
 datarange = [[-0.02,0.02],[-0.02,0.02],[-1,0]]
 
 
 # Creates histograms to compare the target and output data
-fig, axs = plt.subplots(3, 3, figsize=(6, 12))
+fig, axs = plt.subplots(3, 3, figsize=(12, 12))
 for i in range(3):
     # 2D histograms showing trends in the data
     axs[0,i].hist2d(target_data[:,i], output[:,i], bins=100, range=[datarange[i],datarange[i]], cmap="viridis", label="Output vs Target")
@@ -49,6 +68,56 @@ for i in range(3):
     axs[2,i].set_xlabel(f"Variable {i} Target")
     axs[2,i].set_ylabel(f"Variable {i} Difference")
 
+plt.show()
+plt.savefig(outGraphFile)
+
+# Creates histograms to compare theta, phi and mag target and output data
+fig2, axs2 = plt.subplots(3, 3, figsize=(12, 12))
+
+thetarange = [np.pi-0.01,np.pi]
+phirange = [-np.pi,np.pi]
+magrange = [0,1]
+
+thetadiffrange = [-0.02,0.02]
+phidiffrange = [-np.pi,np.pi]
+magdiffrange = [-0.2,0.2]
+
+# 2D histograms showing trends in the data
+axs2[0,0].hist2d(out_theta, in_theta, bins=100, range=[thetarange,thetarange], cmap="viridis", label="Output vs Target")
+axs2[0,0].set_xlabel("Theta Target")
+axs2[0,0].set_ylabel("Theta Output")
+
+axs2[0,1].hist2d(out_phi, in_phi, bins=100, range=[phirange,phirange], cmap="viridis", label="Output vs Target")
+axs2[0,1].set_xlabel("Phi Target")
+axs2[0,1].set_ylabel("Phi Output")
+
+axs2[0,2].hist2d(out_mag, in_mag, bins=100, range=[magrange,magrange], cmap="viridis", label="Output vs Target")
+axs2[0,2].set_xlabel("Mag Target")
+axs2[0,2].set_ylabel("Mag Output")
+
+axs2[1,0].hist(thetadiff, bins=100, alpha=0.5, range=thetadiffrange, label="Difference")
+axs2[1,0].set_xlabel("Theta Difference")
+axs2[1,0].set_ylabel("Counts")
+
+axs2[1,1].hist(phidiff, bins=100, alpha=0.5, range=phidiffrange, label="Difference")
+axs2[1,1].set_xlabel("Phi Difference")
+axs2[1,1].set_ylabel("Counts")
+
+axs2[1,2].hist(magdiff, bins=100, alpha=0.5, range=magdiffrange, label="Difference")
+axs2[1,2].set_xlabel("Mag Difference")
+axs2[1,2].set_ylabel("Counts")
+
+axs2[2,0].hist2d(in_theta, thetadiff, bins=100, range=[thetarange,thetadiffrange], cmap="viridis", label="Difference vs Target")
+axs2[2,0].set_xlabel("Theta Target")
+axs2[2,0].set_ylabel("Theta Difference")
+
+axs2[2,1].hist2d(in_phi, phidiff, bins=100, range=[phirange,phidiffrange], cmap="viridis", label="Difference vs Target")
+axs2[2,1].set_xlabel("Phi Target")
+axs2[2,1].set_ylabel("Phi Difference")
+
+axs2[2,2].hist2d(in_mag, magdiff, bins=100, range=[magrange,magdiffrange], cmap="viridis", label="Difference vs Target")
+axs2[2,2].set_xlabel("Mag Target")
+axs2[2,2].set_ylabel("Mag Difference")
 
 plt.show()
-plt.savefig("output_vs_target.png")
+plt.savefig(outGraphFile2)

4d176ba566191abe897449fa9be3f5782472782c
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-09-19 18:49:33 -0400

    Revert ".rootlogon.C: respect ROOT_BUILD_DIR"
    This reverts commit 6effdb455f29f95595f14678afc954a05b626d65.

--- .rootlogon.C
+++ .rootlogon.C
@@ -2,13 +2,6 @@
 	// top-level include-dir
   gROOT->ProcessLine(".include include");
 
-  // setup a local build directory so we don't polute our source code with
-  // ROOT dictionaries etc. if desired
-  const char* build_dir = gSystem->Getenv("ROOT_BUILD_DIR");
-  if (build_dir) {
-    gSystem->SetBuildDir(build_dir);
-  }
-
   R__LOAD_LIBRARY(fmt)
   R__LOAD_LIBRARY(HepMC3)
 

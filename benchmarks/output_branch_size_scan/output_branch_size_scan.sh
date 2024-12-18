e154ebd0c8dc2e7aa7d38761620d77e4205e66b0
Sakib Rahman <rahmans@myumanitoba.ca>
2024-12-18 11:34:06 -0500

    Placeholder benchmarking shell wrapper script
    Get branch sizes for a current campaign file and the default file and then compare them in a plot

---
+++ benchmarks/output_branch_size_scan/output_branch_size_scan.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+root -q -b root://dtn-eic.jlab.org//work/eic2/EPIC/RECO/<current_file> -e 'for (auto b : *events->GetListOfLeaves()) { if (events->GetBranch(b->GetName()) == nullptr) continue; cout << events->GetBranch(b->GetName())->GetTotalSize() << " " << b->GetName() << endl; }' | sort -n > branch_size_current.txt
+root -q -b root://dtn-eic.jlab.org//work/eic2/EPIC/RECO/<default_file> -e 'for (auto b : *events->GetListOfLeaves()) { if (events->GetBranch(b->GetName()) == nullptr) continue; cout << events->GetBranch(b->GetName())->GetTotalSize() << " " << b->GetName() << endl; }' | sort -n > branch_size_default.txt
+python plot_branch_sizes.py -c branch_size_current.txt -d branch_size_default.txt

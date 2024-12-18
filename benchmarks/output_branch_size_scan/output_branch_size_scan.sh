548fe5cd8693fbaec657455c66b0703d26f7137f
Sakib Rahman <rahmans@myumanitoba.ca>
2024-12-18 11:57:02 -0500

    Change name of current campaign file to extract branch sizes from

--- benchmarks/output_branch_size_scan/output_branch_size_scan.sh
+++ benchmarks/output_branch_size_scan/output_branch_size_scan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-root -q -b root://dtn-eic.jlab.org//work/eic2/EPIC/RECO/<current_file> -e 'for (auto b : *events->GetListOfLeaves()) { if (events->GetBranch(b->GetName()) == nullptr) continue; cout << events->GetBranch(b->GetName())->GetTotalSize() << " " << b->GetName() << endl; }' | sort -n > branch_size_current.txt
+root -q -b eicrecon.tree.edm4eic.root -e 'for (auto b : *events->GetListOfLeaves()) { if (events->GetBranch(b->GetName()) == nullptr) continue; cout << events->GetBranch(b->GetName())->GetTotalSize() << " " << b->GetName() << endl; }' | sort -n > branch_size_current.txt
 root -q -b root://dtn-eic.jlab.org//work/eic2/EPIC/RECO/<default_file> -e 'for (auto b : *events->GetListOfLeaves()) { if (events->GetBranch(b->GetName()) == nullptr) continue; cout << events->GetBranch(b->GetName())->GetTotalSize() << " " << b->GetName() << endl; }' | sort -n > branch_size_default.txt
 python plot.py -c branch_size_current.txt -d branch_size_default.txt

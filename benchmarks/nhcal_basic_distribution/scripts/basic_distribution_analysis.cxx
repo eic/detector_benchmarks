7388620c59a3912c95d5732a7e7f9b6775f7c321
EIC Container Build Service <msieczczynski@gmail.com>
2025-11-09 14:30:23 -0500

    Add calculating energy resolution to basic_distribution

--- benchmarks/nhcal_basic_distribution/scripts/basic_distribution_analysis.cxx
+++ benchmarks/nhcal_basic_distribution/scripts/basic_distribution_analysis.cxx
@@ -61,46 +61,88 @@
 
 #include <edm4eic/vector_utils_legacy.h>
 #include <edm4hep/Vector3f.h>
-
-// #include "FileList.h"
-// #include "EICutil.h"
-// #include "BasicUtil.h"
-
-// #pragma link C++ class vector<edm4hep::MCParticleData>+;
-// #pragma link C++ class vector<podio::ObjectID>+;
-// #pragma link C++ class vector<edm4hep::SimCalorimeterHitData>+;
+#include <root/TStyle.h>
 
 using namespace std;
 using namespace ROOT;
 using namespace TMath;
 using namespace edm4hep;
 
-inline string addPrefixAfterSlash(const string& path,
-                                       const string& prefix) {
+inline string addPrefixAfterSlash(const string& path, const string& prefix) {
     const auto slash = path.find_last_of('/');
     if (slash == string::npos)
         return prefix + path;
     return path.substr(0, slash + 1) + prefix + path.substr(slash + 1);
 }
 
-int basic_distribution_analysis(const string &filename, string outname_pdf, string outname_png) 
+string changeExtension(const string& path, const string& new_ext)
 {
+    size_t pos = path.find_last_of('.');
+    if (pos != string::npos)
+        return path.substr(0, pos) + new_ext;
+    return path + new_ext;
+}
+
+int basic_distribution_analysis(const string &filename, string outname_png, string outname_root) 
+{
+    constexpr double NBINS = 50;
+    constexpr double MIN_X_MM = -3000, MAX_X_MM = 3000;
+    constexpr double MIN_Y_MM = -3000, MAX_Y_MM = 3000;
+    constexpr double MIN_Z_MM = -4500, MAX_Z_MM = -3600;
+    constexpr double MIN_R_MM = 0, MAX_R_MM = 3000;
+    constexpr double MIN_TOTAL_ENERGY_GEV = 0, MAX_TOTAL_ENERGY_GEV = 0.3;
+    constexpr double MIN_HITS = 0, MAX_HITS = 300;
+    constexpr double MIN_LAYER_HITS = 0, MAX_LAYER_HITS = 50;
+    constexpr double MIN_LAYER_ENERGY_GEV = 0, MAX_LAYER_ENERGY_GEV = 0.2;
+    constexpr double MIN_KINETIC_GEV = 0, MAX_KINETIC_GEV = 6;
+
+    gStyle->SetTitleSize(0.045, "XYZ");
+    gStyle->SetLabelSize(0.04, "XYZ");
+    gStyle->SetPadLeftMargin(0.15);
+    gStyle->SetPadRightMargin(0.15);
+    gStyle->SetPadBottomMargin(0.15);
+    gStyle->SetPadTopMargin(0.10);
+
+    string outname_pdf = changeExtension(outname_png, ".pdf");
+
     podio::ROOTReader *reader = new podio::ROOTReader();
     reader->openFile(filename);
     unsigned nEvents = reader->getEntries("events");
     cout << "Number of events: " << nEvents << endl;
 
-    vector<double> xPosition;
-    vector<double> yPosition;
-    vector<double> zPosition;
-    vector<double> rPosition;
-    vector<double> energy;
-    vector<double> totalEventEnergy;
-    vector<double> totalEventHits;
-    vector<double> zLayerValues;
-    vector<int> nHitsPerLayer;
-    vector<double> nEnergyPerLayer;
-    vector<double> kineticEnergy;
+    TH1D *h_energyTotal = new TH1D("h_energyTotal", "Total Energy per Event; Energy per Event [GeV]", 
+                                    NBINS, MIN_TOTAL_ENERGY_GEV, MAX_TOTAL_ENERGY_GEV);
+    
+    TH2D *h_layerEnergy = new TH2D("h_layerEnergy", "Energy in Layers; Z [mm]; Energy [GeV]", 
+                                    NBINS, MIN_Z_MM, MAX_Z_MM, NBINS, MIN_LAYER_ENERGY_GEV, MAX_LAYER_ENERGY_GEV);
+    
+    TProfile *p_layerEnergy = new TProfile("p_layerEnergy", "Energy in Layers; Z [mm]; Mean Energy [GeV]",
+                                            NBINS, MIN_Z_MM, MAX_Z_MM);
+    
+    TH1D *h_hitCount = new TH1D("h_hitCount", "Hits per Event; Hits per Event; N", 
+                                NBINS, MIN_HITS, MAX_HITS);
+    
+    TH2D *h_layerHits = new TH2D("h_layerHits", "Hits in Layers; Z [mm]; Hits", 
+                                  NBINS, MIN_Z_MM, MAX_Z_MM, NBINS, MIN_LAYER_HITS, MAX_LAYER_HITS);
+    
+    TProfile *p_layerHits = new TProfile("p_layerHits", "Hits in Layers; Z [mm]; Mean Hits",
+                                          NBINS, MIN_Z_MM, MAX_Z_MM);
+    
+    TH2D *h_XYPos = new TH2D("h_XYPos", "Hits position;X [mm];Y [mm]", 
+                              NBINS, MIN_X_MM, MAX_X_MM, NBINS, MIN_Y_MM, MAX_Y_MM);
+    
+    TH2D *h_ZRPos = new TH2D("h_ZRPos", "Hits position;Z [mm];R [mm]", 
+                              NBINS, MIN_Z_MM, MAX_Z_MM, NBINS, MIN_R_MM, MAX_R_MM);
+    
+    TH2D *h_XYEnergy = new TH2D("h_XYEnergy", "Hits energy;X [mm];Y [mm]", 
+                                 NBINS, MIN_X_MM, MAX_X_MM, NBINS, MIN_Y_MM, MAX_Y_MM);
+
+    TH2D *h_energyRes = new TH2D("h_energyRes", "Kinetic energy vs sum hit energy;Ekin [GeV]; Ehit sum [GeV]", 
+                                  NBINS, MIN_KINETIC_GEV, MAX_KINETIC_GEV,
+                                  NBINS, MIN_TOTAL_ENERGY_GEV, MAX_TOTAL_ENERGY_GEV);
+    
+    TProfile *p_energyRes = new TProfile("p_energyRes", "Kinetic energy vs sum hit energy",
+                                          NBINS, MIN_KINETIC_GEV, MAX_KINETIC_GEV, "s");
     
     for (unsigned ev = 0; ev < nEvents; ev++) 
     {
@@ -114,116 +156,54 @@ int basic_distribution_analysis(const string &filename, string outname_pdf, stri
         podio::Frame frame(std::move(frameData));
 
         auto& hits = frame.get<edm4hep::SimCalorimeterHitCollection>("HcalEndcapNHits");
-        auto& mcCol  = frame.get<edm4hep::MCParticleCollection>("MCParticles");
+        auto& mcCol = frame.get<edm4hep::MCParticleCollection>("MCParticles");
         if (!hits.isValid() && !mcCol.isValid())    
         {
             cerr << "HcalEndcapNHits or MCParticles collection is not valid!" << endl;
         }
 
+        double Ekin = mcCol[0].getEnergy() - mcCol[0].getMass();
+
         map<double, pair<int, double>> layerData;
         double totalEnergy = 0;
         int totalHits = 0;
 
-        double Ekin = mcCol[0].getEnergy(); //- mcCol[0].getMass();
-        kineticEnergy.push_back(Ekin);
-
         for (const auto& hit : hits) 
         {
             totalHits++;
-            energy.push_back(hit.getEnergy());
             totalEnergy += hit.getEnergy();
 
             auto pos = hit.getPosition();
-            xPosition.push_back(pos.x);
-            yPosition.push_back(pos.y);
-            zPosition.push_back(pos.z);
-            rPosition.push_back(sqrt(pos.x * pos.x + pos.y * pos.y));
+            double r = sqrt(pos.x * pos.x + pos.y * pos.y);
+            
+            h_XYPos->Fill(pos.x, pos.y);
+            h_ZRPos->Fill(pos.z, r);
+            h_XYEnergy->Fill(pos.x, pos.y, hit.getEnergy());
 
             double zBin = round(pos.z);
             layerData[zBin].first++;         
             layerData[zBin].second += hit.getEnergy();
         }
 
-        totalEventEnergy.push_back(totalEnergy);
-        totalEventHits.push_back(totalHits);
+        h_energyTotal->Fill(totalEnergy);
+        h_hitCount->Fill(totalHits);
+        h_energyRes->Fill(Ekin, totalEnergy);
+        p_energyRes->Fill(Ekin, totalEnergy);
 
         for (const auto& [zValue, stats] : layerData)
         {
-            zLayerValues.push_back(zValue);
-            nHitsPerLayer.push_back(stats.first);
-            nEnergyPerLayer.push_back(stats.second);
+            h_layerHits->Fill(zValue, stats.first);
+            p_layerHits->Fill(zValue, stats.first);
+            h_layerEnergy->Fill(zValue, stats.second);
+            p_layerEnergy->Fill(zValue, stats.second);
         }
-
     }
-
-    auto minmax_xPosition        = minmax_element(xPosition.begin(), xPosition.end());
-    auto minmax_yPosition        = minmax_element(yPosition.begin(), yPosition.end());
-    auto minmax_zPosition        = minmax_element(zPosition.begin(), zPosition.end());
-    auto minmax_rPosition        = minmax_element(rPosition.begin(), rPosition.end());
-    auto minmax_totalEventEnergy = minmax_element(totalEventEnergy.begin(), totalEventEnergy.end());
-    auto minmax_totalEventHits   = minmax_element(totalEventHits.begin(), totalEventHits.end());
-    auto minmax_energy           = minmax_element(energy.begin(), energy.end());
-    auto minmax_zLayerValues     = minmax_element(zLayerValues.begin(), zLayerValues.end());
-    auto minmax_nHitsPerLayer    = minmax_element(nHitsPerLayer.begin(), nHitsPerLayer.end());
-    auto minmax_nEnergyPerLayer  = minmax_element(nEnergyPerLayer.begin(), nEnergyPerLayer.end());
-    auto minmax_kineticEnergy    = minmax_element(kineticEnergy.begin(), kineticEnergy.end());
-
-    int nbins = nEvents/1000;
-
-    TH1D *h_energyTotal = new TH1D("h_energyTotal", "Total Energy per Event; Energy per Event [Gev]", 
-                                    nbins/2, *minmax_totalEventEnergy.first, *minmax_totalEventEnergy.second);
-    TH2D *h_layerEnergy = new TH2D("h_layerEnergy", "Energy in Layers (Z); Z [mm]; Energy [GeV]", 
-                                    nbins/5, *minmax_zLayerValues.first, *minmax_zLayerValues.second, 
-                                    nbins/4, *minmax_nEnergyPerLayer.first, *minmax_nEnergyPerLayer.second);
-    TProfile *p_layerEnergy = new TProfile("p_layerEnergy", "Energy in Layers (Z); Z [mm]; Mean Energy [Gev]",
-                                    nbins/5, *minmax_zLayerValues.first, *minmax_zLayerValues.second);
-    TH1D *h_hitCount    = new TH1D("h_hitCount", "Number of Hits per Event; Hits per Event; N", 
-                                    nbins/2, *minmax_totalEventHits.first, *minmax_totalEventHits.second);
-    TH2D *h_layerHits   = new TH2D("h_layerHits", "Number of Hits in Layers (Z); Layer(Z); Hits", 
-                                    nbins/5, *minmax_zLayerValues.first, *minmax_zLayerValues.second, 
-                                    *minmax_nHitsPerLayer.second, *minmax_nHitsPerLayer.first, *minmax_nHitsPerLayer.second);
-    TProfile *p_layerHits = new TProfile("p_layerHits", "Number of Hits in Layers (Z); Z [mm]; Mean Hits",
-                                    nbins/5, *minmax_zLayerValues.first, *minmax_zLayerValues.second);
-    TH2D *h_XYPos       = new TH2D("h_XYPos", "Hits position X,Y;X [mm];Y [mm]", 
-                                    nbins, *minmax_xPosition.first, *minmax_xPosition.second, 
-                                    nbins, *minmax_yPosition.first, *minmax_yPosition.second);
-    TH2D *h_ZRPos       = new TH2D("h_ZRPos", "Hits position Z,R;Z [mm];R [mm]", 
-                                    nbins/5, *minmax_zPosition.first, *minmax_zPosition.second, 
-                                    nbins, *minmax_rPosition.first, *minmax_rPosition.second);
-    TH2D *h_XYEnergy    = new TH2D("h_XYEnergy", "Hits energy X,Y;X [mm];Y [mm]", 
-                                    nbins, *minmax_xPosition.first, *minmax_xPosition.second, 
-                                    nbins, *minmax_yPosition.first, *minmax_yPosition.second);
-    TH2D *h_EnergyRes    = new TH2D("h_EnergyRes", "Kinetic energy vs sum hit energy;Ekin [GeV]; Ehit sum [GeV]", 
-                                    nbins, *minmax_kineticEnergy.first, *minmax_kineticEnergy.second,
-                                    nbins, *minmax_totalEventEnergy.first, *minmax_totalEventEnergy.second );
-    TProfile *p_EnergyRes = new TProfile("p_EnergyRes", "Kinetic energy vs sum hit energy",
-                                    nbins, *minmax_kineticEnergy.first, *minmax_kineticEnergy.second);
     
-    for(int i = 0; i < xPosition.size(); i++)
-    {   
-        h_XYPos->Fill(xPosition[i], yPosition[i]);
-        h_ZRPos->Fill(zPosition[i], rPosition[i]);
-        h_XYEnergy->Fill(xPosition[i], yPosition[i], energy[i]);
-    }
-
-    for (int i = 0; i < zLayerValues.size(); i++)
-    {
-        h_layerHits->Fill(zLayerValues[i], nHitsPerLayer[i]);
-        p_layerHits->Fill(zLayerValues[i], nHitsPerLayer[i]);
-
-        h_layerEnergy->Fill(zLayerValues[i], nEnergyPerLayer[i]);
-        p_layerEnergy->Fill(zLayerValues[i], nEnergyPerLayer[i]);
-    }
-
-    for(int i = 0; i < nEvents; i++)
-    {
-        h_energyTotal->Fill(totalEventEnergy[i]);
-        h_hitCount->Fill(totalEventHits[i]);
-        h_EnergyRes->Fill(kineticEnergy[i], totalEventEnergy[i]);
-        p_EnergyRes->Fill(kineticEnergy[i], totalEventEnergy[i]);
-    }
-
-    gStyle->SetPadLeftMargin(0.13);
+    TFile *outFile = new TFile(outname_root.c_str(), "RECREATE");
+    h_energyRes->Write();
+    p_energyRes->Write();
+    outFile->Close();
+    delete outFile;
 
     TCanvas *c_evLayers = new TCanvas("c_evLayers", "c_evLayers", 1600, 800);
     c_evLayers->Divide(2,2);
@@ -231,15 +211,17 @@ int basic_distribution_analysis(const string &filename, string outname_pdf, stri
     h_energyTotal->Draw();
     c_evLayers->cd(2);
     h_layerEnergy->Draw("COLZ");
+    p_layerEnergy->SetLineWidth(3); p_layerEnergy->SetLineColor(kRed); p_layerEnergy->SetMarkerColor(kRed);
     p_layerEnergy->Draw("SAME");
     c_evLayers->cd(3);
     h_hitCount->Draw();
     c_evLayers->cd(4);
     h_layerHits->Draw("COLZ");
-    p_layerHits->Draw("SAME S");
+    p_layerHits->SetLineWidth(3); p_layerHits->SetLineColor(kRed); p_layerHits->SetMarkerColor(kRed);
+    p_layerHits->Draw("SAME");
 
-    c_evLayers->SaveAs(outname_pdf.c_str());
     c_evLayers->SaveAs(outname_png.c_str());
+    c_evLayers->SaveAs(outname_pdf.c_str());
 
     TCanvas *c_hit_posE = new TCanvas("c_hit_posE", "c_hit_posE", 1600, 800);
     c_hit_posE->Divide(2,2);
@@ -250,11 +232,13 @@ int basic_distribution_analysis(const string &filename, string outname_pdf, stri
     c_hit_posE->cd(3);
     h_XYEnergy->Draw("COLZ");
     c_hit_posE->cd(4);
-    h_EnergyRes->Draw("COLZ");
-    p_EnergyRes->Draw("SAME");
-
-    c_hit_posE->SaveAs(addPrefixAfterSlash(outname_pdf, "hit_posE_").c_str());
+    
     c_hit_posE->SaveAs(addPrefixAfterSlash(outname_png, "hit_posE_").c_str());
+    c_hit_posE->SaveAs(addPrefixAfterSlash(outname_pdf, "hit_posE_").c_str());
+
+    delete reader;
+    delete c_evLayers;
+    delete c_hit_posE;
 
     return 0;
 }
---
+++ benchmarks/nhcal_basic_distribution/scripts/basic_distribution_energy_resolution.cxx
@@ -0,0 +1,157 @@
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "ROOT/RDataFrame.hxx"
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TRandom3.h>
+#include <TFile.h>
+#include <TChain.h>
+#include <TTree.h>
+#include <TMath.h>
+#include <TVector3.h>
+#include <TCanvas.h>
+
+#include "TROOT.h"
+#include "TRandom.h"
+#include "TH3.h"
+
+
+#include "DD4hep/Detector.h"
+#include "DDRec/CellIDPositionConverter.h"
+
+#include <podio/Frame.h>
+#include <podio/CollectionBase.h>
+#include "podio/ROOTReader.h"
+//#include <podio/ROOTFrameReader.h>
+#include "podio/CollectionIDTable.h"
+#include "podio/ObjectID.h"
+
+#include "edm4hep/MCParticleCollection.h"
+#include "edm4hep/MCParticleCollectionData.h"
+#include "edm4hep/MCParticle.h"
+#include "edm4hep/MCParticleData.h"
+
+#include "edm4hep/SimCalorimeterHitCollectionData.h"
+#include "edm4hep/SimCalorimeterHitCollection.h"
+#include "edm4hep/SimCalorimeterHitData.h"
+#include "edm4hep/SimCalorimeterHit.h"
+
+#include "edm4hep/CalorimeterHit.h"
+#include "edm4hep/CalorimeterHitCollectionData.h"
+#include "edm4hep/CalorimeterHitCollection.h"
+#include "edm4hep/CalorimeterHitData.h"
+#include "edm4hep/CalorimeterHit.h"
+#include "edm4hep/CalorimeterHitObj.h"
+
+#include "edm4eic/ClusterCollection.h"
+#include "edm4eic/Cluster.h"
+#include "edm4eic/ClusterData.h"
+
+#include "edm4eic/CalorimeterHit.h"
+#include "edm4eic/CalorimeterHitCollectionData.h"
+#include "edm4eic/CalorimeterHitCollection.h"
+#include "edm4eic/CalorimeterHitData.h"
+#include "edm4eic/CalorimeterHit.h"
+#include "edm4eic/CalorimeterHitObj.h"
+
+
+#include <edm4eic/vector_utils_legacy.h>
+#include <edm4hep/Vector3f.h>
+#include <root/TStyle.h>
+#include <root/TGraphErrors.h>
+
+using namespace std;
+using namespace ROOT;
+using namespace TMath;
+using namespace edm4hep;
+
+string changeExtension(const string& path, const string& new_ext)
+{
+    size_t pos = path.find_last_of('.');
+    if (pos != string::npos)
+        return path.substr(0, pos) + new_ext;
+    return path + new_ext;
+}
+
+int basic_distribution_energy_resolution(const string &filename, string outname_png){
+        
+    gStyle->SetTitleSize(0.045, "XYZ");
+    gStyle->SetLabelSize(0.04, "XYZ");
+    gStyle->SetPadLeftMargin(0.15);
+    gStyle->SetPadRightMargin(0.15);
+    gStyle->SetPadBottomMargin(0.15);
+    gStyle->SetPadTopMargin(0.10);
+
+    string outname_pdf = changeExtension(outname_png, ".pdf");
+
+    TFile *file = TFile::Open(filename.c_str(), "READ");
+    if (!file || file->IsZombie()) 
+    {
+        cerr << "Cannot open file: " << filename << endl;
+        return 1;
+    }
+
+    TH2D *h_energyRes = (TH2D*)file->Get("h_energyRes");
+    TProfile *p_energyRes = (TProfile*)file->Get("p_energyRes");
+
+    if (!h_energyRes || !p_energyRes) 
+    {
+        cerr << "Cannot retrieve histograms from file" << endl;
+        file->Close();
+        return 1;
+    }
+
+    TGraphErrors *g_resolution = new TGraphErrors();
+
+    for (int i = 1; i <= p_energyRes->GetNbinsX(); i++) {
+        double Ekin = p_energyRes->GetBinCenter(i);
+        double mean = p_energyRes->GetBinContent(i);
+        double rms = p_energyRes->GetBinError(i); 
+        int entries = p_energyRes->GetBinEntries(i);
+        
+        if (entries < 10 || mean <= 0) continue;
+        
+        int n = g_resolution->GetN();
+        g_resolution->SetPoint(n, Ekin, rms/mean);
+        g_resolution->SetPointError(n, 0, rms/mean * sqrt(1.0/entries));
+    }
+
+    // Fit σ/E = a/√E ⊕ b
+    TF1 *fit = new TF1("fit", "sqrt([0]*[0]/x + [1]*[1])", 0.1, 6);
+    fit->SetParameters(0.5, 0.05);
+    fit->SetParNames("a (stochastic)", "b (constant)");
+    g_resolution->Fit(fit, "R");
+
+    TCanvas *c = new TCanvas("c", "Energy Resolution", 1600, 800);
+    c->Divide(2, 1);
+    
+    c->cd(1);
+    h_energyRes->Draw("COLZ");
+    p_energyRes->SetLineWidth(3); 
+    p_energyRes->SetLineColor(kRed); 
+    p_energyRes->SetMarkerColor(kRed);
+    p_energyRes->Draw("SAME");
+
+    c->cd(2);
+    g_resolution->SetTitle("Energy Resolution;E_{kin} [GeV];#sigma/E");
+    g_resolution->SetMarkerStyle(20);
+    g_resolution->Draw("AP");
+    fit->Draw("SAME");
+    
+    TLegend *leg = new TLegend(0.45, 0.65, 0.80, 0.80);
+    leg->AddEntry(g_resolution, "Data", "p");
+    leg->AddEntry(fit, Form("Fit: #sqrt{(%.3f/#sqrt{E})^{2} + (%.3f)^{2}}", 
+                           fit->GetParameter(0), fit->GetParameter(1)), "l");
+    leg->Draw();
+
+    c->SaveAs(outname_png.c_str());
+    c->SaveAs(outname_pdf.c_str());
+
+    file->Close();
+
+    return 0;
+}

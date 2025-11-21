a603c6f40c4f8f759adbf72d222b3c673f2284cc
simonge <simon.gardner@glasgow.ac.uk>
2025-11-21 11:11:22 +0000

    Add tagger acceptance plots

--- benchmarks/lowq2_reconstruction/reconstructionAnalysis.C
+++ benchmarks/lowq2_reconstruction/reconstructionAnalysis.C
@@ -8,15 +8,19 @@
 #include "edm4eic/ReconstructedParticleCollection.h"
 #include "TCanvas.h"
 #include "TStyle.h"
+#include "TMath.h"
 
-void reconstructionAnalysis(TString inFile             = "/home/simong/EIC/detector_benchmarks_anl/sim_output/lowq2_reconstruction/analysis/Low-Q2_retrained_Particles_new.eicrecon.edm4hep.root",
-                            float   beamEnergy         = 18.0,
-                            TString outFile            = "reconstruction_results.root",
-                            TString momentumCanvasName = "momentum_resolution.png",
-                            TString energyThetaPhiCanvasName = "energy_theta_phi_resolution.png",
-                            TString relationCanvasName = "relation_resolution.png",
-                            TString resolutionGraphsCanvasName = "resolution_graphs.png",
-                            std::string particleCollectionName = "TaggerTrackerReconstructedParticles") {
+void reconstructionAnalysis( TString inFile                       = "/home/simong/EIC/detector_benchmarks_anl/sim_output/lowq2_reconstruction/analysis/Low-Q2_retrained_Particles_new.eicrecon.edm4hep.root",
+                             float   beamEnergy                   = 18.0,
+                             TString outFile                      = "reconstruction_results.root",
+                             TString momentumCanvasName           = "momentum_resolution.png",
+                             TString energyThetaPhiCanvasName     = "energy_theta_phi_resolution.png",
+                             TString relationCanvasName           = "relation_resolution.png",
+                             TString resolutionGraphsCanvasName   = "resolution_graphs.png",
+                             TString acceptanceCanvasName         = "acceptance_canvas.png",
+                             TString energyQ2acceptanceCanvasName = "energy_Q2_acceptance_canvas.png",
+                             TString xQ2acceptanceCanvasName      = "x_Q2_acceptance_canvas.png",
+                             std::string particleCollectionName   = "TaggerTrackerReconstructedParticles") {
 
     //Set ROOT style    
     gStyle->SetPadLeftMargin(0.1);  // Set left margin
@@ -33,33 +37,76 @@ void reconstructionAnalysis(TString inFile             = "/home/simong/EIC/detec
 
     ROOT::RDataFrame d0("events",inFile, {"MCParticles",particleCollectionName});
 
-    auto filterDF = d0.Define("SimParticles", "MCParticles[MCParticles.generatorStatus==1 && MCParticles.PDG==11]")
-                      .Filter("SimParticles.size()==1")
-                      .Filter(particleCollectionName+".size()==1");
+    // Base MC electron definitions prior to reconstruction filter
+    auto baseDF = d0.Define("SimParticles", "MCParticles[MCParticles.generatorStatus==1 && MCParticles.PDG==11]")
+                    .Define("hasElectron", "SimParticles.size()==1")
+                    .Define("mc_momentum", "hasElectron ? SimParticles[0].momentum : MCParticles[0].momentum")
+                    .Define("px_mc", "mc_momentum.x")
+                    .Define("py_mc", "mc_momentum.y")
+                    .Define("pz_mc", "mc_momentum.z")
+                    .Define("E_mc", "sqrt(px_mc*px_mc + py_mc*py_mc + pz_mc*pz_mc + (hasElectron ? SimParticles[0].mass*SimParticles[0].mass : MCParticles[0].mass*MCParticles[0].mass))")
+                    .Define("theta_mc", "std::atan2(std::sqrt(px_mc*px_mc + py_mc*py_mc), pz_mc)")
+                    .Define("phi_mc_rad", "std::atan2(py_mc, px_mc)")
+                    .Define("phi_mc", "TMath::RadToDeg()*phi_mc_rad")
+                    // Q2 using initial electron beam along -z
+                    .Define("Q2_mc", [beamEnergy](double px, double py, double pz, double E){
+                        double Ee = beamEnergy; // electron beam energy (along -z)
+                        double dE = Ee - E;
+                        double dqx = px;
+                        double dqy = py;
+                        double dqz = -Ee - pz;
+                        double q2 = (dE*dE - (dqx*dqx + dqy*dqy + dqz*dqz));
+                        return -q2; // ensure positive Q2
+                    }, {"px_mc","py_mc","pz_mc","E_mc"})
+                    // W using full collider initial state: electron (Ee,0,0,-Ee) + proton (Ep,0,0,+pz_p)
+                    .Define("W_mc", [beamEnergy](double Q2, double px, double py, double pz, double E){
+                        const double Mp = 0.9382720813; // proton mass GeV
+                        const double Ep = 275.0; // proton beam energy GeV
+                        double pz_p = std::sqrt(Ep*Ep - Mp*Mp);
+                        double Ee = beamEnergy;
+                        // q = p_e0 - p_e
+                        double qE = Ee - E;
+                        double qx = -px;
+                        double qy = -py;
+                        double qz = -Ee - pz;
+                        // p_p0 + q
+                        double totalE = Ep + qE;
+                        double totalPx = qx;
+                        double totalPy = qy;
+                        double totalPz = pz_p + qz;
+                        double W2 = totalE*totalE - (totalPx*totalPx + totalPy*totalPy + totalPz*totalPz);
+                        return W2 > 0 ? std::sqrt(W2) : 0.0;
+                    }, {"Q2_mc","px_mc","py_mc","pz_mc","E_mc"})
+                    .Define("log10Q2_mc", "Q2_mc>0 ? std::log10(Q2_mc) : -10.0")
+                    // Bjorken x = Q2 / (2 * Mp * nu), where nu = Ee - E_mc
+                    .Define("x_bj", [beamEnergy](double Q2, double E){
+                        const double Mp = 0.9382720813; // proton mass GeV
+                        double Ee = beamEnergy;
+                        double nu = Ee - E;
+                        return (nu > 0 && Q2 > 0) ? Q2 / (2.0 * Mp * nu) : 1e-10;
+                    }, {"Q2_mc","E_mc"})
+                    .Define("log10x_mc", "x_bj>0 ? std::log10(x_bj) : -10.0");
+
+    // Denominator: events with a valid scattered electron
+    auto denomDF = baseDF.Filter("hasElectron");
+    // Numerator: additionally require single reconstructed particle
+    auto filterDF = denomDF.Filter(particleCollectionName+".size()==1");
 
     // Plot x,y,z momentum resolution as a function of the MCParticle momentum
     auto momentumDF = filterDF
         .Define("reco_momentum", particleCollectionName+"[0].momentum")
-        .Define("mc_momentum", "SimParticles[0].momentum")
         .Define("px_rec", "reco_momentum.x")
         .Define("py_rec", "reco_momentum.y")
         .Define("pz_rec", "reco_momentum.z")
-        .Define("px_mc", "mc_momentum.x")
-        .Define("py_mc", "mc_momentum.y")
-        .Define("pz_mc", "mc_momentum.z")
-        // Calculate theta and phi for reco and MC
+        // Calculate theta and phi for reco
         .Define("theta_rec", "std::atan2(std::sqrt(px_rec*px_rec + py_rec*py_rec), pz_rec)")
-        .Define("theta_mc", "std::atan2(std::sqrt(px_mc*px_mc + py_mc*py_mc), pz_mc)")
         .Define("phi_rec_rad", "std::atan2(py_rec, px_rec)")
-        .Define("phi_mc_rad", "std::atan2(py_mc, px_mc)")
         .Define("phi_rec", "TMath::RadToDeg()*phi_rec_rad")
-        .Define("phi_mc", "TMath::RadToDeg()*phi_mc_rad")
         // Calculate resolutions
         .Define("px_diff", "(px_rec - px_mc)")
         .Define("py_diff", "(py_rec - py_mc)")
         .Define("pz_res", "(pz_rec - pz_mc)/pz_mc")
         .Define("E_rec", particleCollectionName+"[0].energy")
-        .Define("E_mc", "std::sqrt(px_mc*px_mc + py_mc*py_mc + pz_mc*pz_mc + SimParticles[0].mass*SimParticles[0].mass)")
         .Define("E_res", "(E_rec - E_mc)/E_mc")
         .Define("theta_diff", "(theta_rec - theta_mc)")
         .Define("phi_diff", "TMath::RadToDeg()*ROOT::VecOps::DeltaPhi(phi_rec_rad, phi_mc_rad)");
@@ -120,6 +167,93 @@ void reconstructionAnalysis(TString inFile             = "/home/simong/EIC/detec
     auto phi_diff_vs_E_Hist       = momentumDF.Histo2D({"phi_diff_vs_E", "phi difference vs E reconstructed; E reconstructed [GeV]; phi difference [rad]", energyBins, energyRange[0], energyRange[1], resolutionBins, phiResolutionRange[0], phiResolutionRange[1]}, "E_rec", "phi_diff");
     auto phi_diff_vs_theta_Hist   = momentumDF.Histo2D({"phi_diff_vs_theta", "phi difference vs theta reconstructed; theta reconstructed [rad]; phi difference [deg]", thetaBins, thetaRange[0], thetaRange[1], resolutionBins, phiResolutionRange[0], phiResolutionRange[1]}, "theta_rec", "phi_diff");
     auto phi_diff_vs_phi_Hist     = momentumDF.Histo2D({"phi_diff_vs_phi", "phi difference vs phi reconstructed; phi reconstructed [deg]; phi difference [deg]", phiBins, phiRange[0], phiRange[1], resolutionBins, phiResolutionRange[0], phiResolutionRange[1]}, "phi_rec", "phi_diff");
+
+    // Acceptance histograms (denominator vs numerator)
+    auto E_all_Hist     = denomDF.Histo1D({"E_all",     "MC Electron Energy; E [GeV]; Entries", energyBins, energyRange[0], energyRange[1]}, "E_mc");
+    auto theta_all_Hist = denomDF.Histo1D({"theta_all", "MC Electron Theta; theta [rad]; Entries", thetaBins, thetaRange[0], thetaRange[1]}, "theta_mc");
+    auto phi_all_Hist   = denomDF.Histo1D({"phi_all",   "MC Electron Phi; phi [deg]; Entries", phiBins, phiRange[0], phiRange[1]}, "phi_mc");
+    auto log10Q2_all_Hist = denomDF.Histo1D({"log10Q2_all", "MC log10(Q^{2}); log10(Q^{2}) [GeV^{2}]; Entries", 100, -10.0, 0.0}, "log10Q2_mc");
+    auto W_all_Hist     = denomDF.Histo1D({"W_all",     "MC W; W [GeV]; Entries", energyBins, 0.0, 5.0}, "W_mc");
+
+    auto E_acc_Hist     = filterDF.Histo1D({"E_acc",     "Accepted Electron Energy; E [GeV]; Entries", energyBins, energyRange[0], energyRange[1]}, "E_mc");
+    auto theta_acc_Hist = filterDF.Histo1D({"theta_acc", "Accepted Electron Theta; theta [rad]; Entries", thetaBins, thetaRange[0], thetaRange[1]}, "theta_mc");
+    auto phi_acc_Hist   = filterDF.Histo1D({"phi_acc",   "Accepted Electron Phi; phi [deg]; Entries", phiBins, phiRange[0], phiRange[1]}, "phi_mc");
+    auto log10Q2_acc_Hist = filterDF.Histo1D({"log10Q2_acc", "Accepted log10(Q^{2}); log10(Q^{2}) [GeV^{2}]; Entries", 100, -10.0, 0.0}, "log10Q2_mc");
+    auto W_acc_Hist     = filterDF.Histo1D({"W_acc",     "Accepted W; W [GeV]; Entries", energyBins, 0.0, 5.0}, "W_mc");
+
+    // 2D acceptance: log10Q2 vs E
+    auto log10Q2_vs_E_all_Hist = denomDF.Histo2D({"log10Q2_vs_E_all", "MC log10(Q^{2}) vs E; E [GeV]; log10(Q^{2})", energyBins, energyRange[0], energyRange[1], 100, -10.0, 0.0}, "E_mc", "log10Q2_mc");
+    auto log10Q2_vs_E_acc_Hist = filterDF.Histo2D({"log10Q2_vs_E_acc", "Accepted log10(Q^{2}) vs E; E [GeV]; log10(Q^{2})", energyBins, energyRange[0], energyRange[1], 100, -10.0, 0.0}, "E_mc", "log10Q2_mc");
+
+    // 2D acceptance: log10Q2 vs log10x
+    auto log10Q2_vs_log10x_all_Hist = denomDF.Histo2D({"log10Q2_vs_log10x_all", "MC log10(Q^{2}) vs log10(x); log10(x); log10(Q^{2})", 100, -10.0, 0.0, 100, -10.0, 0.0}, "log10x_mc", "log10Q2_mc");
+    auto log10Q2_vs_log10x_acc_Hist = filterDF.Histo2D({"log10Q2_vs_log10x_acc", "Accepted log10(Q^{2}) vs log10(x); log10(x); log10(Q^{2})", 100, -10.0, 0.0, 100, -10.0, 0.0}, "log10x_mc", "log10Q2_mc");
+
+    TH1D* hE_acceptance     = (TH1D*)E_acc_Hist->Clone("hE_acceptance");     hE_acceptance->Divide(E_all_Hist.GetPtr());
+    TH1D* hTheta_acceptance = (TH1D*)theta_acc_Hist->Clone("hTheta_acceptance"); hTheta_acceptance->Divide(theta_all_Hist.GetPtr());
+    TH1D* hPhi_acceptance   = (TH1D*)phi_acc_Hist->Clone("hPhi_acceptance");   hPhi_acceptance->Divide(phi_all_Hist.GetPtr());
+    TH1D* hLog10Q2_acceptance = (TH1D*)log10Q2_acc_Hist->Clone("hLog10Q2_acceptance"); hLog10Q2_acceptance->Divide(log10Q2_all_Hist.GetPtr());
+    TH1D* hW_acceptance     = (TH1D*)W_acc_Hist->Clone("hW_acceptance");     hW_acceptance->Divide(W_all_Hist.GetPtr());
+
+    // 2D acceptance
+    TH2D* hLog10Q2_vs_E_acceptance = (TH2D*)log10Q2_vs_E_acc_Hist->Clone("hLog10Q2_vs_E_acceptance");
+    hLog10Q2_vs_E_acceptance->Divide(log10Q2_vs_E_all_Hist.GetPtr());
+
+    TH2D* hLog10Q2_vs_log10x_acceptance = (TH2D*)log10Q2_vs_log10x_acc_Hist->Clone("hLog10Q2_vs_log10x_acceptance");
+    hLog10Q2_vs_log10x_acceptance->Divide(log10Q2_vs_log10x_all_Hist.GetPtr());
+
+    auto styleAcc = [](TH1* h,const char* title){
+        h->SetTitle(title);
+        h->GetYaxis()->SetTitle("Acceptance");
+        h->SetMinimum(0.0);
+        h->SetMaximum(1.05);
+        h->SetMarkerStyle(20);
+        h->SetMarkerColor(kBlue+1);
+        h->SetLineColor(kBlue+1);
+    };
+    styleAcc(hE_acceptance,     "Electron Acceptance vs E; E [GeV]");
+    styleAcc(hTheta_acceptance, "Electron Acceptance vs theta; theta [rad]");
+    styleAcc(hPhi_acceptance,   "Electron Acceptance vs phi; phi [deg]");
+    styleAcc(hLog10Q2_acceptance, "Electron Acceptance vs log10(Q^{2}); log10(Q^{2})");
+    styleAcc(hW_acceptance,     "Electron Acceptance vs W; W [GeV]");
+
+    // Style 2D acceptance
+    hLog10Q2_vs_E_acceptance->SetTitle("Electron Acceptance: log10(Q^{2}) vs E; E [GeV]; log10(Q^{2})");
+    hLog10Q2_vs_E_acceptance->GetZaxis()->SetTitle("Acceptance");
+    hLog10Q2_vs_E_acceptance->SetMinimum(0.0);
+    hLog10Q2_vs_E_acceptance->SetMaximum(1.0);
+
+    hLog10Q2_vs_log10x_acceptance->SetTitle("Electron Acceptance: log10(Q^{2}) vs log10(x); log10(x); log10(Q^{2})");
+    hLog10Q2_vs_log10x_acceptance->GetZaxis()->SetTitle("Acceptance");
+    hLog10Q2_vs_log10x_acceptance->SetMinimum(0.0);
+    hLog10Q2_vs_log10x_acceptance->SetMaximum(1.0);
+
+    TCanvas *cAcceptance1D = new TCanvas("acceptance_1D_canvas", "Electron Acceptance (1D)", 2400, 1600);
+    cAcceptance1D->Divide(3,2);
+    cAcceptance1D->cd(1); hE_acceptance->Draw("E1");
+    cAcceptance1D->cd(2); hTheta_acceptance->Draw("E1");
+    cAcceptance1D->cd(3); hPhi_acceptance->Draw("E1");
+    cAcceptance1D->cd(4); hLog10Q2_acceptance->Draw("E1");
+    cAcceptance1D->cd(5); hW_acceptance->Draw("E1");
+    cAcceptance1D->SetGrid();
+    cAcceptance1D->Update();
+    cAcceptance1D->SaveAs(acceptanceCanvasName);
+
+    TCanvas *cAcceptance2D = new TCanvas("acceptance_2D_canvas", "Electron Acceptance: log10(Q^{2}) vs E", 1200, 1000);
+    cAcceptance2D->cd();
+    gStyle->SetOptStat(0);
+    hLog10Q2_vs_E_acceptance->Draw("COLZ");
+    cAcceptance2D->SetRightMargin(0.15);
+    cAcceptance2D->Update();
+    cAcceptance2D->SaveAs(energyQ2acceptanceCanvasName);
+
+    TCanvas *cAcceptanceXQ2 = new TCanvas("acceptance_xQ2_canvas", "Electron Acceptance: log10(Q^{2}) vs log10(x)", 1200, 1000);
+    cAcceptanceXQ2->cd();
+    gStyle->SetOptStat(0);
+    hLog10Q2_vs_log10x_acceptance->Draw("COLZ");
+    cAcceptanceXQ2->SetRightMargin(0.15);
+    cAcceptanceXQ2->Update();
+    cAcceptanceXQ2->SaveAs(xQ2acceptanceCanvasName);
 
     // Create canvas for momentum component plots
     TCanvas *cMomentum = new TCanvas("momentum_canvas", "Momentum Resolution", 3000, 1600);
@@ -250,14 +384,14 @@ void reconstructionAnalysis(TString inFile             = "/home/simong/EIC/detec
     hTheta_vs_E_stddev->SetMinimum(0); // Adjust minimum for better visibility
     hTheta_vs_E_stddev->Draw();
     cResolutionGraphs->cd(3);
-    hPhi_vs_theta_mean->SetTitle("Mean Phi Offset vs theta MC; theta MC [rad]; Mean Phi Offset [rad]");
+    hPhi_vs_theta_mean->SetTitle("Mean Phi Offset vs theta MC; theta MC [rad]; Mean Phi Offset [deg]");
     hPhi_vs_theta_mean->SetMarkerStyle(20);
     hPhi_vs_theta_mean->SetMarkerColor(kBlue);
     hPhi_vs_theta_mean->SetMaximum(5); // Adjust maximum for better visibility
     hPhi_vs_theta_mean->SetMinimum(-5); // Adjust minimum for better visibility
     hPhi_vs_theta_mean->Draw();
     cResolutionGraphs->cd(6);
-    hPhi_vs_theta_stddev->SetTitle("Std Dev Phi Offset vs theta MC; theta MC [rad]; Std Dev Phi Offset [rad]");
+    hPhi_vs_theta_stddev->SetTitle("Std Dev Phi Offset vs theta MC; theta MC [rad]; Std Dev Phi Offset [deg]");
     hPhi_vs_theta_stddev->SetMarkerStyle(20);
     hPhi_vs_theta_stddev->SetMarkerColor(kRed);
     hPhi_vs_theta_stddev->SetMaximum(60); // Adjust maximum for better visibility
@@ -273,6 +407,9 @@ void reconstructionAnalysis(TString inFile             = "/home/simong/EIC/detec
     cEnergyThetaPhi->Write();
     cResolutionVsMC->Write();
     cResolutionGraphs->Write();
+    cAcceptance1D->Write();
+    cAcceptance2D->Write();
+    cAcceptanceXQ2->Write();
     px_Hist->Write();
     py_Hist->Write();
     pz_Hist->Write();
@@ -294,6 +431,14 @@ void reconstructionAnalysis(TString inFile             = "/home/simong/EIC/detec
     phi_diff_vs_E_Hist->Write();
     phi_diff_vs_theta_Hist->Write();
     phi_diff_vs_phi_Hist->Write();
+    // Write acceptance related histograms
+    E_all_Hist->Write(); E_acc_Hist->Write(); hE_acceptance->Write();
+    theta_all_Hist->Write(); theta_acc_Hist->Write(); hTheta_acceptance->Write();
+    phi_all_Hist->Write(); phi_acc_Hist->Write(); hPhi_acceptance->Write();
+    log10Q2_all_Hist->Write(); log10Q2_acc_Hist->Write(); hLog10Q2_acceptance->Write();
+    W_all_Hist->Write(); W_acc_Hist->Write(); hW_acceptance->Write();
+    log10Q2_vs_E_all_Hist->Write(); log10Q2_vs_E_acc_Hist->Write(); hLog10Q2_vs_E_acceptance->Write();
+    log10Q2_vs_log10x_all_Hist->Write(); log10Q2_vs_log10x_acc_Hist->Write(); hLog10Q2_vs_log10x_acceptance->Write();
 
     hE_vs_E_mean->Write();
     hE_vs_E_stddev->Write();
